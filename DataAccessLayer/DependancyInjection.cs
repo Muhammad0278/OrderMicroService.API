a6ddfbd8974f8863e43293425cc180ef9f5440c1
Muhammad Arslan <131705335+Muhammad0278@users.noreply.github.com>
2025-12-18 17:55:49 +0100

    Adding MONGODB

--- DataAccessLayer/DependancyInjection.cs
+++ DataAccessLayer/DependancyInjection.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
 
 
 namespace eCommerce.OrderMicroservice.DataAccessLayer;
@@ -8,11 +11,18 @@ public static class DependancyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+       string constr = configuration.GetConnectionString("MongoDB")!;
 
-        //services.AddDbContext<OrderDbContext>(options =>
-        //    options.UseSqlServer(configuration.GetConnectionString("OrderDatabase")));
-        //// Register repositories
-        //services.AddScoped<IOrderRepository, OrderRepository>();
+       string connectionstring= constr.Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST"))
+            .Replace("MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
+
+        services.AddSingleton<IMongoClient>(new MongoClient(connectionstring));
+
+        services.AddScoped<IMongoDatabase>(provider =>
+        {
+            IMongoClient client = provider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("OrdersDatabase");
+        });
         return services;
     }
 }
